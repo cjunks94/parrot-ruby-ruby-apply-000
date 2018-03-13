62d2c86e8fe6ee007bf0c6d88f79dd0408a42c77
cjunks94 <cjunks94@gmail.com>
2018-03-13 20:32:55 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(*str)
-  newstr = "Squawk!"
+  newstr="Squawk!"
+  if str == "Pretty bird!"
+  puts str
+  return str
+else
   puts newstr
   return newstr
 end
